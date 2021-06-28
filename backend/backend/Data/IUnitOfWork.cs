226fa3cd8b6a7c1dc45f04d5ee0e4895f90c08dc
Lamija Koca <klamija99@gmail.com>
2021-06-28 14:50:45 +0200

    interface for every another

---
+++ backend/backend/Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using backend.Data.EmployeeRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Data
+{
+    public interface IUnitOfWork
+    {
+        public IEmployeeData IEmployee { get; set; }
+        Task<bool> Complete();
+    }
+}
