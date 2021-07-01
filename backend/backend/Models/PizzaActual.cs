02388795dedf75005acf5da4d335551880eb8d68
Lamija Koca <klamija99@gmail.com>
2021-07-01 16:24:35 +0200

    Added PizzaActual

--- backend/backend/Models/PizzaActual.cs
+++ backend/backend/Models/PizzaActual.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,12 @@ namespace backend.Models
 {
     public class PizzaActual
     {
+        [Key]
+        [ForeignKey("Pizza")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+        public virtual Pizza Pizza { get; set; }
     }
 }
