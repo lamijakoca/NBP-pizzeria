660f8569f5b92c6632fb03fb7ef6e7b862eed673
Lamija Koca <klamija99@gmail.com>
2021-07-01 16:07:28 +0200

    added storage

---
+++ backend/backend/Models/Storage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Storage
+    {
+        public Storage()
+        {
+            this.Ingredients = new HashSet<Ingredient>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
