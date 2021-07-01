b11d6381528e28033e75963307b5460ce555fb47
Lamija Koca <klamija99@gmail.com>
2021-07-01 16:09:17 +0200

    Added Worker page

--- frontend/src/components/Worker.js
+++ frontend/src/components/Worker.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import logo from '../assets/logo.png'
+import "./styles/workerPage.css"
 
 function Worker(){
     return(
-        <h2>Worker Page</h2>
+        <div className="workerMain">
+            <img src={logo} alt="Pizzeria"/>
+            <h2>Worker Page</h2>
+        </div>
     )
 }
 
