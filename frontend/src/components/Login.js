7cd787162c8f38147bbeffdf201af1d02f987b76
Lamija Koca <klamija99@gmail.com>
2021-07-01 12:39:42 +0200

    select iz antd ne radi, pa koristim obicnu react komponentu

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,75 +1,95 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Form, Input, Button, message } from 'antd';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Form, Input, Button, message } from "antd";
+import logo from "../assets/logo.png";
+import backend from "../utils";
+import axios from "axios";
 import "antd/dist/antd.css";
 import "./styles/login.css";
-import logo from '../assets/logo.png'
-import backend from '../utils'; 
-import axios from 'axios';
 
-function Login(){
-    const history = useHistory();
-    //const [user, setUser] = useState("employee");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [isLogin, setIsLogin] = useState(false);
+function Login() {
+  const history = useHistory();
+  const [type, setType] = useState("Select");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
 
-    //const [form] = Form.useForm();
-    // const onTypeChange = (value) => {
-    //     if(value == "customer"){
-    //         setUser("customer");
-    //     }
-    //     else setUser("employee");
-    // }
+  const onTypeChange = (event) => {
+    setType(event.target.value);
+    console.log(type);
+  };
 
-    const loginSubmit = async () => {
-        const User = {username, password};
-        axios.post(`${backend}/api/auth/login`, User)
-             .then((res) => {
-                 const {token} = res.data;
-                 localStorage.setItem("token", token);
-                 setIsLogin(true);
-             })
+  const loginSubmit = async () => {
+    const User = { username, password };
+    if (type === "employee") {
+      axios.post(`${backend}/api/auth/login`, User).then((res) => {
+        const { token } = res.data;
+        localStorage.setItem("token", token);
+        setIsLogin(true);
+      });
+    } else if (type === "customer") {
+      axios.post(`${backend}/api/auth/login/customer`, User).then((res) => {
+        const { token } = res.data;
+        localStorage.setItem("token", token);
+        setIsLogin(true);
+      });
     }
+  };
 
-    if (isLogin == true){
-        history.push("/employee");
-        console.log("okej tu si");
-        message.success("You are logged in");
-        setTimeout(function () {
-            window.location.reload(1);
-          }, 1500);
-    }
-    return(
-        <div className="loginDiv">
-            <img src={logo} className="logo" alt="pizzeria"/>
-            <Form className="formDiv" onFinish={loginSubmit}>
-                <Form.Item 
-                label="Username" 
-                name="username" 
-                rules={[{required: true, message: "Please input your username!"}]}
-                >
-                    <Input onChange = {(e) => setUsername(e.target.value)}/>
-                </Form.Item>
+  if (isLogin === true && type === "employee") {
+    history.push("/worker");
+    console.log("okej tu si");
+    message.success("You are logged in");
+    setTimeout(function () {
+      window.location.reload(1);
+    }, 1500);
+  } else if (isLogin === true && type === "customer") {
+    history.push("/customer");
+    console.log("Welcome customer!");
+  }
 
-                <Form.Item 
-                label="Password" 
-                name="password" 
-                rules={[{required: true, min: 8 ,message:"Please input your password!"}]}
-                >
-                    <Input.Password onChange = {(e) => setPassword(e.target.value)}/>
-                </Form.Item>
+  return (
+    <div className="loginDiv">
+      <img src={logo} className="logo" alt="pizzeria" />
 
-                <Button 
-                className="submitButton"
-                type="primary" 
-                htmlType="submit">
-                    Submit
-                </Button>
-                <p className="regPlease">If you don't have an account, please Register.</p>
-            </Form>
-        </div>
-    )
+      <Form className="formDiv" onFinish={loginSubmit}>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input onChange={(e) => setUsername(e.target.value)} />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            { required: true, min: 8, message: "Please input your password!" },
+          ]}
+        >
+          <Input.Password onChange={(e) => setPassword(e.target.value)} />
+        </Form.Item>
+
+        <Form.Item name="type" label="Type" rules={[{ required: true }]}>
+          {/* antd select ne radi  */}
+          <select onChange={onTypeChange} className="selectOption" value={type}>
+            <option value="Select">Select</option>
+            <option value="customer">Customer</option>
+            <option value="employee">Employee</option>
+          </select>
+        </Form.Item>
+
+        <Button className="submitButton" type="primary" htmlType="submit">
+          Submit
+        </Button>
+
+        <p className="regPlease">
+          If you don't have an account, please Register.
+        </p>
+      </Form>
+    </div>
+  );
 }
 
 export default Login;
