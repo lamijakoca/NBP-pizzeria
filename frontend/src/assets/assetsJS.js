d5ada9ada1d038d66669ca172433d5d107571927
Lamija Koca <klamija99@gmail.com>
2021-07-04 15:32:00 +0200

    exportujem sve slike

---
+++ frontend/src/assets/assetsJS.js
@@ -0,0 +1,12 @@
+import capricciosa from './capricciosa.jpg'
+import fungi from './fungi.jpg'
+
+export{
+    capricciosa,
+    fungi,
+}
+
+// export const capricciosa = require("./capricciosa.jpg");
+// export const carbonara = require("./carbonara.jpg");
+// export const fungi = require("./fungi.jpg");
+// export const margarita = require("./margarita.jpg");
