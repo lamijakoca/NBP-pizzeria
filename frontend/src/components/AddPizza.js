e3dcea502590d0a9288522a9b8886d6e8482f427
Lamija Koca <klamija99@gmail.com>
2021-07-06 01:44:46 +0200

    file input ocajni koji ima neki bug

--- frontend/src/components/AddPizza.js
+++ frontend/src/components/AddPizza.js
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, message } from "antd";
 import { useHistory } from "react-router-dom";
-import { Upload, message } from "antd";
-import { InboxOutlined } from '@ant-design/icons';
 import axios from "axios";
 import database from "../utils";
+import './styles/addPizzaPage.css'
+import logo from '../assets/logo.png'
 
 function AddPizza() {
   const history = useHistory();
-  
-  const {Dragger} = Upload;
 
   const [name, setName] = useState("");
-  const [image, setImage] = useState("capricciosa");
-  const [isPost, setIsPost] = useState(false);
-
-  // if(isPost){history.push("/worker");}
+  const [image, setImage] = useState("");
 
   const createCard = () => {
     const token = localStorage.getItem("token");
@@ -24,24 +19,39 @@ function AddPizza() {
       axios.post(`${database}/api/pizza`, newPizza, {
         headers: { "Authorization": token },
       });
-      setIsPost(true)
     }
   };
 
   return (
-    <div>
-      <h2>New Pizza</h2>
-      <h3>Title / Name of Pizza</h3>
+    <div className="newPizzaDiv">
+      <img src={logo} alt="logo"/>
+      <h2 className="addPizzaH2">Here you can add new pizza!</h2>
+      <h4>Title / Name of Pizza</h4>
+      
       <Form onFinish={createCard}>
         <Input
           className="inputTitle"
           onChange={(e) => setName(e.target.value)}
         />
 
+        <input 
+        onChange={(e)=>{
+          var needSlice = e.target.value;
+          var sliceNow = needSlice.slice(12, needSlice.length - 4);
+          setImage(sliceNow)
+        }}
+        type="file" 
+        accept="image/png, image/jpeg, image/jpg"
+        className="imageInput"
+        />
+        
         <Button
-          type="default"
+          type="primary"
           htmlType="submit"
           className="saveBtn"
+          onClick={() => {
+            message.success("Successfully saved!")
+          }}
         >
           Save
         </Button>
