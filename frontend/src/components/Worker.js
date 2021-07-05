d1b3c2f163b7828105a26c43bc487dfae2fa2807
Lamija Koca <klamija99@gmail.com>
2021-07-06 01:40:12 +0200

    imports, delete option etc

--- frontend/src/components/Worker.js
+++ frontend/src/components/Worker.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import * as images from '../assets/assetsJS'
 import {useHistory} from 'react-router-dom';
 import logo from '../assets/logo.png';
-import { Card, Button } from 'antd';
+import { Card, Button, message } from 'antd';
+import {DeleteOutlined} from '@ant-design/icons';
 import "./styles/workerPage.css";
 import database from '../utils';
 import axios from 'axios';
@@ -10,22 +11,26 @@ import axios from 'axios';
 function Worker(){
     const history = useHistory();
 
-    const [size, setSize] = useState("Medium");
     const [pizza, setPizza] = useState([]);
     const [token, setToken] = useState("");
-
+  
     const getPizzas = async(token) => {
         const res = await axios.get(`${database}/api/pizza`, {
-            headers: {"Authorization": token}
-            
+            headers: {"Authorization": token}    
         })
         setPizza(res.data);
         //console.log(res.data[0]);
     }
-    const handleChange = (value) =>{
-        setSize(value);
-    }
 
+    const deletePizza = async (id) => {
+        if(token){
+            await axios.delete(`${database}/api/pizza/${id}`,{
+                headers: {"Authorization" : token}
+            })
+            message.success("Successfully deleted!");
+        }
+    }
+    
     useEffect(() => {
         const token = localStorage.getItem("token");
         setToken(token);
@@ -46,19 +51,23 @@ function Worker(){
             </Button>
             {
                 pizza.map((p) => (
-                    <div key={p.id}>
+                    <div key={p.name}>
                         <Card title={p.name} className="cards">
                             <img className="pizzaImg" src={images[p.image]} alt="Pizza"/>
                             {/* {console.log(p.image)} */}
-                            <p>Ingredients</p>
+                            <p style={{marginLeft:"83%", fontWeight:"bold"}}>Ingredients</p>
+                            <ul title="Ingredients">
+                                <li>meso</li>
+                                <li>paradajz sos</li>
+                                <li>trapist</li>
+                                <li>origano</li>
+                            </ul>
                             <div className="footer">
-                                <select onChange={handleChange} className="selectSize">
-                                    <option value="small">Small</option>
-                                    <option value="medium">Medium</option>
-                                    <option value="big">Big</option>
-                                </select>
-                                <Button type="default" className="addToCart">
-                                    Add to Cart
+                                <Button 
+                                className="deleteBtn"
+                                onClick={() => {deletePizza(p.id)}}
+                                >
+                                    <DeleteOutlined />
                                 </Button>
                             </div>
                         </Card>
