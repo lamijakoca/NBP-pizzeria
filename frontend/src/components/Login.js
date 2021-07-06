3a0148f4413523ba5f3242772806978a51a4a434
Lamija Koca <klamija99@gmail.com>
2021-07-06 12:08:07 +0200

    push na odgovarajucu stranicu (admin/worker)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -18,36 +18,41 @@ function Login() {
     setType(event.target.value);
   };
 
+  const howToPush = () =>{
+    if (type === "employee"){
+      history.push('/worker')
+    }
+    else if (type === "admin"){
+      history.push('/admin')
+    }
+  }
   const loginSubmit = async () => {
     const User = { username, password };
-    if (type === "employee") {
+    if (type === "employee" || type === "admin") {
       await axios.post(`${backend}/api/auth/login`, User)
           .then((res) => {
             const token = res.data;
             localStorage.setItem("token", token);
             setIsLogin(true);
-      });
+      }, howToPush());
     } else if (type === "customer") {
       await axios.post(`${backend}/api/auth/login/customer`, User)
             .then((res) => {
             const token = res.data;
             localStorage.setItem("token", token);
             setIsLogin(true);
-      });
+      }, history.push('/customer'));
     }
   };
 
-  if (isLogin === true && type === "employee") {
-    history.push("/worker");
-    console.log("okej tu si");
-    message.success("You are logged in");
-    setTimeout(function () {
-      window.location.reload(1);
-    }, 1500);
-  } else if (isLogin === true && type === "customer") {
-    history.push("/customer");
-    console.log("Welcome customer!");
-  }
+  // if (isLogin === true && type === "employee") {
+  //   history.push("/worker");
+  //   console.log("okej tu si");
+  //   message.success("You are logged in", 2);
+  // } else if (isLogin === true && type === "customer") {
+  //   history.push("/customer");
+  //   console.log("Welcome customer!");
+  // }
 
   return (
     <div className="loginDiv">
@@ -77,6 +82,7 @@ function Login() {
           <select onChange={onTypeChange} className="selectOption" value={type}>
             <option value="Select">Select</option>
             <option value="customer">Customer</option>
+            <option value="admin">Admin</option>
             <option value="employee">Employee</option>
           </select>
         </Form.Item>
